751897256b142a62436dfac5b806d9ccae5308c4
Júnior Michalski <michalskijr@gmail.com>
2021-12-20 22:43:24 -0300

    Criado os components Header e Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-import { PaginaInicial } from "./pages/PaginaInicial"
+import { PaginaInicial } from "./pages/PaginaInicial";
+import { Header } from "./components/header";
+import { Footer } from "./components/footer"
 
 function App() {
   return (
     <div className='App'>
+      <Header />
       <PaginaInicial />
+      <Footer />
     </div>
   );
 }
